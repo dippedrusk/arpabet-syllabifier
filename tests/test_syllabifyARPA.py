4ef76120e2755b5affff5c3f9ab561b21db583f2
vgautam <vgautam@sfu.ca>
2020-05-11 22:28:09 -0700

    dump the old tests

--- tests/test_syllabifyARPA.py
+++
@@ -1,5 +0,0 @@
-from syllabifier import syllabifyARPA
-
-def test_syllabifyARPA():
-    test_string = 'HH AE NG M AE N'
-    assert syllabifyARPA(test_string, return_list=True) == ['HH AE NG', 'M AE N']
