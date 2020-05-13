1cd89c2ab836a7d12325b90dd0eabd37b75bad15
vgautam <vgautam@sfu.ca>
2020-05-12 19:03:03 -0700

    moar tests

--- tests/test_syllabifier.py
+++ tests/test_syllabifier.py
@@ -1,4 +1,11 @@
+import itertools
 from syllabifier import syllabifyARPA
+from syllabifier import CONSONANTS
+from syllabifier import PHONESET
+
+VOWELS = PHONESET - CONSONANTS
+legal_codas = CONSONANTS - {'HH', 'W', 'Y'}
+legal_onsets = CONSONANTS - {'NG'}
 
 def test_syllabifyARPA():
     test_string = 'HH AE NG M AE N'
@@ -35,3 +42,19 @@ def test_array():
 def test_weird_array():
     test_array = ['K AE', 'T']
     assert not syllabifyARPA(test_array, return_list=True, silence_warnings=True)
+
+def test_CVC_syllables():
+    for syllable in itertools.product(legal_onsets, VOWELS, legal_codas):
+        assert syllabifyARPA(list(syllable), return_list=True)
+
+def test_SZ_extension():
+    not_sz_extendable = {'S', 'SH', 'Z', 'ZH', 'CH', 'JH'}
+    sz_extendable_codas = legal_codas - not_sz_extendable
+    for syllable in itertools.product(legal_onsets, VOWELS, sz_extendable_codas):
+        s_extended = list(syllable) + ['S']
+        z_extended = list(syllable) + ['Z']
+        assert (syllabifyARPA(s_extended, return_list=True, silence_warnings=True) !=
+                syllabifyARPA(z_extended, return_list=True, silence_warnings=True))
+
+def test_TD_extension():
+    assert True
