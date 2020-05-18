84360e31f89938f7b7fadcb30088474594be39d8
Vasundhara Gautam <15020857+vgautam@users.noreply.github.com>
2020-05-18 00:15:20 -0700

    More tests (#9)
    * moar tests
    
    * actually, don't import the constants in the top-level __init__.py

--- tests/test_syllabifier.py
+++ tests/test_syllabifier.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import itertools
+import pytest
 from syllabifier import syllabifyARPA
 from syllabifier.constants import CONSONANTS
 from syllabifier.constants import PHONESET
@@ -14,18 +15,63 @@ def test_syllabifyARPA():
     assert syllabifyARPA(test_string) == ['HH AE NG', 'M AE N']
 
 
-def test_unsyllabifiable():
-    test_string = 'M G L AA'
-    assert not syllabifyARPA(test_string, silence_warnings=True)
-
-
-def test_empty():
-    test_string = ''
+def test_non_ARPABET_phones():
+    test_string = 'banana'
+    with pytest.raises(ValueError, match='contains non-ARPABET phones'):
+        syllabifyARPA(test_string)
     assert not syllabifyARPA(test_string, silence_warnings=True)
 
 
 def test_no_vowel():
+    test_string = 'K R F JH'
+    with pytest.raises(ValueError, match='no vowel in'):
+        syllabifyARPA(test_string)
+    assert not syllabifyARPA(test_string, silence_warnings=True)
     test_string = 'K S'
+    with pytest.raises(ValueError, match='no vowel in'):
+        syllabifyARPA(test_string)
+    assert not syllabifyARPA(test_string, silence_warnings=True)
+
+
+def test_bad_onset_cluster():
+    test_string = 'M G L AA'
+    with pytest.raises(ValueError, match='Bad onset cluster'):
+        syllabifyARPA(test_string)
+    assert not syllabifyARPA(test_string, silence_warnings=True)
+
+
+def test_NG_onset():
+    test_string = 'NG OW'
+    with pytest.raises(ValueError, match='Bad onset cluster'):
+        syllabifyARPA(test_string)
+    assert not syllabifyARPA(test_string, silence_warnings=True)
+
+
+def test_bad_coda():
+    test_string = 'AE G R P'
+    with pytest.raises(ValueError, match='Bad coda cluster'):
+        syllabifyARPA(test_string)
+    assert not syllabifyARPA(test_string, silence_warnings=True)
+
+
+def test_length_5_coda():
+    test_string = 'AE N G L S F'
+    with pytest.raises(ValueError, match='Bad coda cluster'):
+        syllabifyARPA(test_string)
+    assert not syllabifyARPA(test_string, silence_warnings=True)
+
+
+def test_length_4_coda():
+    test_string = 'AE N S G F'
+    with pytest.raises(ValueError, match='Bad coda cluster'):
+        syllabifyARPA(test_string)
+    assert not syllabifyARPA(test_string, silence_warnings=True)
+    test_string = 'S IH K S TH S'
+    assert syllabifyARPA(test_string) == [test_string]
+
+
+def test_empty():
+    test_string = ''
     assert not syllabifyARPA(test_string, silence_warnings=True)
 
 
@@ -104,19 +150,34 @@ def test_V_syllables():
 
 
 def test_CVCCC_syllables():
-    syllables = ['Y EH L P S', 'K AE L K S', 'W AO R M TH', 'W IH L S T', 'JH IH NG K S']
+    syllables = [
+        'Y EH L P S', 'K AE L K S', 'W AO R M TH', 'W IH L S T', 'JH IH NG K S', 'M IH L K T',
+        'P AH M P T'
+    ]
+    for syllable in syllables:
+        assert syllabifyARPA(syllable) == [syllable]
+
+
+def test_CCVCCC_syllables():
+    syllables = [
+        'K R AE M P T', 'B L IH NG K T', 'T W AE NG G D'
+    ]
     for syllable in syllables:
         assert syllabifyARPA(syllable) == [syllable]
 
 
 def test_CCVCC_syllables():
-    syllables = ['TH W AO R T', 'K L AH T S', 'B L AH N T', 'F Y OW R D', 'G R IH N CH']
+    syllables = [
+        'TH W AO R T', 'K L AH T S', 'B L AH N T', 'F Y OW R D', 'G R IH N CH'
+    ]
     for syllable in syllables:
         assert syllabifyARPA(syllable) == [syllable]
 
 
 def test_s_clusters():
-    syllables = ['S P OW R T', 'S M AY L', 'S F IH NG K S', 'S P Y UW', 'S T AH D', 'S K W EY R']
+    syllables = [
+        'S P OW R T', 'S M AY L', 'S F IH NG K S', 'S P Y UW', 'S T AH D', 'S K W EY R',
+        'S T R AY K']
     for syllable in syllables:
         assert syllabifyARPA(syllable) == [syllable]
     prons_syllabified = {
@@ -134,7 +195,11 @@ def test_long_words():
         'AH S F IH K S IY EY T AH D': ['AH', 'S F IH K', 'S IY', 'EY', 'T AH D'],
         'M AY K R AH S K AA P IH K': ['M AY', 'K R AH', 'S K AA', 'P IH K'],
         'N Y UH M AE T IH K S': ['N Y UH', 'M AE', 'T IH K S'],
-        'F L AE JH AH L EY SH AH N Z': ['F L AE', 'JH AH', 'L EY', 'SH AH N Z']
+        'F L AE JH AH L EY SH AH N Z': ['F L AE', 'JH AH', 'L EY', 'SH AH N Z'],
+        'CH AA R L S T AH N': ['CH AA R L', 'S T AH N'],
+        'JH AH M P T B AE K': ['JH AH M P T', 'B AE K'],
+        'K R UW S CH Y AO F': ['K R UW S', 'CH Y AO F'],
+        'TH AW Z AH N D TH': ['TH AW', 'Z AH N D TH'],
     }
     for pron, syllabification in prons_syllabified.items():
         assert syllabifyARPA(pron) == syllabification
@@ -144,3 +209,8 @@ def test_long_words():
 # TODO:ADD this feature
 #        'S W IY P S T EY K S': ['S W IY P', 'S T EY K S']
 #        'S EH S AH M IY S T R IY T': ['S EH', 'S AH', 'M IY', 'S T R IY T'
+
+# def test_syllabic_consonant_nuclei():
+# TODO: ADD this feature
+# len(syllabifyARPA('B IY T L') == len(syllabifyARPA('B IY T AH L')
+# syllabifyARPA('B IY T L') == ['B IY', 'T L']
